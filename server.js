9f744c9fda2dd0ca78701fddc35d5efed89c87b4
Nasrullo Nurullaev <61620246+nasrullonurullaev@users.noreply.github.com>
2025-02-10 16:09:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ function showPrimes(n) {
   nextPrime:
   for (let i = 2; i < n; i++) {
 
-    // проверяем, является ли i простым числом
     for (let j = 2; j < i; j++) {
       if (i % j == 0) continue nextPrime;
     }
